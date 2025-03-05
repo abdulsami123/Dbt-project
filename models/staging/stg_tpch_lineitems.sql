d71b020c46a771dc784d37ed73051d8d007d6056
Abdul Sami <45204596+abdulsami123@users.noreply.github.com>
2025-03-05 14:32:04 -0600

    Staging files and initial Dbt setup

---
+++ models/staging/stg_tpch_lineitems.sql
@@ -0,0 +1,19 @@
+select
+    {{
+        -- surrogate key generated using function in Dbt
+        -- surrogate key used since this table didn't have its own PK
+        -- This concatenates the specified fields and applies a hash function to produce a unique ID.
+        dbt_utils.generate_surrogate_key([
+            'l_orderkey',
+            'l_linenumber'
+        ])
+    }} as order_item_key,
+	l_orderkey as order_key,
+	l_partkey as part_key,
+	l_linenumber as line_number,
+	l_quantity as quantity,
+	l_extendedprice as extended_price,
+	l_discount as discount_percentage,
+	l_tax as tax_rate
+from
+    {{ source('tpch', 'lineitem') }}
